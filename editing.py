5f14d7652045b2306513b22dd8215978171c4c22
golyzhbina <88280116+golyzhbina@users.noreply.github.com>
2021-09-21 12:14:37 +0500

    Add files via upload

---
+++ editing.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os.path
+import numpy as np
+
+way_to_sample = r"C:\Users\Lenovo\Desktop\уир\Void microCT dataset_Mehdikhani et al\Voids microCT dataset\Tomographic images\Edit1"
+name_of_sample = "0040e.tif"
+sample = Image.open(os.path.join(way_to_sample, name_of_sample))
+sample_as_array = np.asarray(sample)
+
+way_to_images = r"C:\Users\Lenovo\Desktop\уир\Void microCT dataset_Mehdikhani et al\Voids microCT dataset\Tomographic images\[+-45]2s"
+lst_of_names = os.listdir(way_to_images)
+lst_of_names = sorted(lst_of_names)[475:1991]
+
+
+for file_name in lst_of_names:
+    image = Image.open(os.path.join(way_to_images, file_name))
+    image_as_array = np.asarray(image)
+    for i in range(len(image_as_array)):
+        image_as_array[i] = np.array(list(map(lambda x, y: y if y == 255 else x,
+                                              image_as_array[i], sample_as_array[i])))
+
+    file_name = file_name[:4] + "e" + file_name[4:]
+    image_from_array = Image.fromarray(image_as_array)
+    image_from_array.save(os.path.join(way_to_sample, file_name))
+    image.close()
+
+sample.close()
