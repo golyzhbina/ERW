08ddefffc07a1d3a4f2409e602dfd4f416486fa6
LAPTOP-OPMNB0HF\Lenovo <golubrysa@gmail.com>
2021-10-08 17:53:21 +0500

    edit part 3

--- photo editing/editing_part3.py
+++ photo editing/editing_part3.py
@@ -1,22 +1,25 @@
 from PIL import Image
-import os.path
 import numpy as np
+import os
 
-way_to_sample = r"C:\Users\Lenovo\Desktop\уир\Void microCT dataset_Mehdikhani et al\Voids microCT dataset\Tomographic images\Edit1"
-name_of_sample = "0040e.tif"
-sample = Image.open(os.path.join(way_to_sample, name_of_sample))
-sample_as_array = np.asarray(sample)
 
-way_to_images = r"C:\Users\Lenovo\Desktop\уир\Void microCT dataset_Mehdikhani et al\Voids microCT dataset\Tomographic images\[+-45]2s"
-file_name = "0041.tif"
-image = Image.open(os.path.join(way_to_images, "0041.tif"))
-image_as_array = np.asarray(image)
-for i in range(len(image_as_array)):
-    image_as_array[i] = np.array(list(map(lambda x, y: y if y == 255 else x,
-                                          image_as_array[i], sample_as_array[i])))
+way_to_images = r"C:\Users\Lenovo\Desktop\UIR\Void microCT dataset_Mehdikhani et al\Voids microCT dataset\Tomographic images\Edit_part1"
+lst_of_names = os.listdir(way_to_images)
+lst_of_names.sort()
 
-file_name = file_name[:4] + "e" + file_name[4:]
-image_from_array = Image.fromarray(image_as_array)
-image_from_array.save(os.path.join(way_to_images, file_name))
-image.close()
-sample.close()
+way_to_edit_images = r"C:\Users\Lenovo\Desktop\UIR\Void microCT dataset_Mehdikhani et al\Voids microCT dataset\Tomographic images\Edit_part2"
+
+for name in lst_of_names:
+
+        img = Image.open(os.path.join(way_to_images, name))
+        image_as_array = np.asarray(img)
+        for i in range(len(image_as_array)):
+                image_as_array[i] = np.array(list(map(lambda x: 255 if x < 70 else 0, image_as_array[i])))
+
+        from_array_img = Image.fromarray(image_as_array)
+
+        new_name = name[0: 4] + "e" + name[4:]
+        from_array_img.save(os.path.join(way_to_edit_images, new_name))
+
+
+
