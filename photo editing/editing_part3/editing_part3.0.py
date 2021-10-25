38fee324b3eb82801021fbe358a758f6466f591a
LAPTOP-OPMNB0HF\Lenovo <golubrysa@gmail.com>
2021-10-25 16:19:53 +0500

    changes editing on step 3

--- photo editing/editing_part3/editing_part3.0.py
+++ photo editing/editing_part3/editing_part3.0.py
@@ -9,18 +9,25 @@ lst_of_names.sort()
 lst_of_names = [lst_of_names[1]]
 
 way_to_edit_images = r"C:\Users\Lenovo\Desktop\UIR"
+colors = [(57, 0, 3832), (66, 3832, 6742), (55, 6742, 8681)]
 
 for name in lst_of_names:
 
         img = Image.open(os.path.join(way_to_images, name))
         image_as_array = np.asarray(img)
+
+        if 1 <= int(name[-4]) < 3833:
+                k = 57
+        elif 3833 <= int(name[-4]) < 6743:
+                k = 66
+        elif 6743 <= int(name[-4]):
+                k = 55
+
         for i in range(len(image_as_array)):
-                image_as_array[i] = np.array(list(map(lambda x: 255 if x < 70 else 0, image_as_array[i])))
+                image_as_array[i] = np.array(list(map(lambda x: 255 if x < k else 0, image_as_array[i])))
 
         from_array_img = Image.fromarray(image_as_array)
-
-        new_name = "0200.tif"
-        from_array_img.save(os.path.join(way_to_edit_images, new_name))
+        from_array_img.save(os.path.join(way_to_edit_images, name))
 
 
 
