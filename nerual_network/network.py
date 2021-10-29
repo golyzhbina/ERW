d197af343b1f58f12dae53919cd6313bf8ec1b12
LAPTOP-OPMNB0HF\Lenovo <golubrysa@gmail.com>
2021-10-29 09:13:34 +0500

    add shuffle

--- nerual_network/network.py
+++ nerual_network/network.py
@@ -4,14 +4,15 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D
 from tensorflow.keras.metrics import MeanIoU
 from tensorflow.keras import Model
-from numpy import array, asarray
+from numpy import array
 from os import listdir
 from os.path import join
+from random import shuffle
 
 
 # preparing data
-way_to_original_train = r"E:\folder_J\images\jpg\train\images_jpg"
-way_to_edited_train = r"E:\folder_J\images\jpg\train\images_edited_jpg"
+way_to_original_train = r"C:\Users\Lenovo\Desktop\UIR\images\jpg\train\images_jpg"
+way_to_edited_train = r"C:\Users\Lenovo\Desktop\UIR\images\jpg\train\images_edited_jpg"
 
 
 def load_images(way: str, lst_of_name: list) -> array:
@@ -28,23 +29,24 @@ def load_images(way: str, lst_of_name: list) -> array:
 
 
 images_name_train = listdir(way_to_original_train)
+shuffle(images_name_train)
 
 x_train = load_images(way_to_original_train, images_name_train)
 y_train = load_images(way_to_edited_train, images_name_train)
 
 input_layer = Input(shape=(1150, 180, 1), batch_size=7804)
-conv1 = Conv2D(64, (3, 3), padding="same", activation="relu")(input_layer)      # 1150 * 180 * 64
+conv1 = Conv2D(64, (3, 3), padding="same", activation="relu")(input_layer)
 conv1_2 = Conv2D(32, (3, 3), padding="same", activation="relu")(conv1)
-pool1 = MaxPooling2D(pool_size=(2, 2))(conv1_2)                                   # 575 * 90 * 64
+pool1 = MaxPooling2D(pool_size=(2, 2))(conv1_2)
 
-conv2 = Conv2D(32, (3, 3), padding="same", activation="relu")(pool1)            # 575 * 90 * 32
-pool2 = MaxPooling2D(pool_size=(5, 2))(conv2)                                   # 115 * 45 * 32
+conv2 = Conv2D(32, (3, 3), padding="same", activation="relu")(pool1)
+pool2 = MaxPooling2D(pool_size=(5, 2))(conv2)
 up2 = UpSampling2D(size=(5, 2))(pool2)
 
 conc3 = concatenate([pool1, up2])
 up3 = UpSampling2D(size=(2, 2))(conc3)
-conv3 = Conv2D(16, (3, 3), padding="same", activation="relu")(up3)            # 115 * 45 * 16
-pool3 = MaxPooling2D(pool_size=(5, 3))(conv3)                                   # 23 * 15 * 16
+conv3 = Conv2D(16, (3, 3), padding="same", activation="relu")(up3)
+pool3 = MaxPooling2D(pool_size=(5, 3))(conv3)
 
 conv4 = Conv2D(1, (1, 1), padding="same", activation="sigmoid")(conv3)
 
@@ -55,6 +57,7 @@ model.compile(optimizer="adam", loss="binary_crossentropy", metrics=[MeanIoU(num
 his = model.fit(x_train, y_train,batch_size=30, epochs=5, validation_split=0.1)
 
 plt.plot(his.history['loss'])
+plt.plot(his.history['val_loss'])
 plt.show()
 
 images_name_test = listdir(way_to_original_train)
